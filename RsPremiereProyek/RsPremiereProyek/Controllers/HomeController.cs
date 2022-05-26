252490b83bb8869d92af2494521143844a55f5ca
Ricky Ananda Pardomuan Sitorus <75471225+rickyananada@users.noreply.github.com>
2022-05-26 23:34:53 +0700

    first commit

---
+++ RsPremiereProyek/RsPremiereProyek/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RsPremiereProyek.Models;
+namespace RsPremiereProyek.Controllers
+{
+    public class HomeController : Controller
+    {
+        DbproyekteknetEntities db = new DbproyekteknetEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            /* if (Session["IdUsSS"] != null)
+             {
+                 return View(db.TBLPesertaInfoes.ToList());
+             }
+             else
+             {
+                 return RedirectToAction("login", "Home");
+             }*/
+            /*return View(db.TBLPesertaInfoes.ToList());*/
+            /*return RedirectToAction("login", "Home");*/
+            return View(db.TBLUserInfoes.ToList());
+        }
+        public ActionResult Singup()
+        {
+            return View();
+        }
+
+        public ActionResult Awal()
+        {
+            return RedirectToAction("Login", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Singup(TBLUserInfo tblUserInfo)
+        {
+            if (db.TBLUserInfoes.Any(x => x.UsernameUs == tblUserInfo.UsernameUs))
+            {
+                ViewBag.Notification = "This Account has Already existed";
+                return View();
+            }
+            else
+            {
+                db.TBLUserInfoes.Add(tblUserInfo);
+                db.SaveChanges();
+
+                Session["IdUsSS"] = tblUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tblUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        public ActionResult logout()
+        {
+            Session.Clear();
+            return RedirectToAction("login", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(TBLUserInfo tblUserInfo)
+        {
+            var checkLogin = db.TBLUserInfoes.Where(x => x.UsernameUs.Equals(tblUserInfo.UsernameUs) && x.PasswordUs.Equals(tblUserInfo.PasswordUs)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+                Session["IduSS"] = tblUserInfo.IdUs.ToString();
+                Session["UsernameSS"] = tblUserInfo.UsernameUs.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Wrong Username or Password";
+            }
+            return View();
+        }
+
+    }
+}
---
+++ RsPremiereProyek/RsPremiereProyek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RsPremiereProyek.MvcApplication" Language="C#" %>
